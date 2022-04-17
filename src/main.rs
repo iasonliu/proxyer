db3106d0b07652bd2f38bba64bec8b967f99b6fd
xiangxu <iasonliu@gmail.com>
2022-04-17 09:24:45 +0800

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 use anyhow::*;
+use clap::Parser;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Request, Response, Server};
 use std::net::SocketAddr;
 use std::sync::{Arc, RwLock};
+use tracing::{error, info};
+
+#[derive(Parser, Debug)]
+#[clap(name = "proxyer", about = "A http proxyer server")]
+struct Config {
+    #[clap(short = 'l', long = "log", default_value = "debug")]
+    log_level: String,
+    #[clap(short = 'a', long = "addr", default_value = "::1")]
+    addr: String,
+    #[clap(short = 'p', long = "port", default_value = "3000")]
+    port: u16,
+}
 
 fn mutate_request(req: &mut Request<Body>) -> Result<()> {
     for key in [
@@ -25,12 +38,28 @@ fn mutate_request(req: &mut Request<Body>) -> Result<()> {
     Ok(())
 }
 
+async fn shutdown_signal() {
+    // Wait for the CTRL+C signal
+    tokio::signal::ctrl_c()
+        .await
+        .expect("failed to install CTRL+C signal handler");
+}
+
 #[derive(Debug)]
 struct Stats {
     proxied: usize,
 }
 #[tokio::main]
 async fn main() -> Result<()> {
+    let config = Config::parse();
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var(
+            "RUST_LOG",
+            format!("{},hyper=info,mio=info", config.log_level),
+        )
+    }
+    tracing_subscriber::fmt::init();
+
     let https = hyper_rustls::HttpsConnectorBuilder::new()
         .with_native_roots()
         .https_only()
@@ -60,19 +89,20 @@ async fn main() -> Result<()> {
                         stats.write().unwrap().proxied += 1;
                         let client = Arc::clone(&client);
                         mutate_request(&mut req)?;
-                        client
-                            .request(req)
-                            .await
-                            .context("Making request to backend server")
+                        client.request(req).await.context("proxy request")
                     }
                 }
             }))
         }
     });
-    Server::bind(&addr)
-        .serve(make_svc)
-        .await
-        .context("Running server")?;
+    info!("listening on http://{}", addr);
+    let server = Server::bind(&addr).serve(make_svc);
+    // And now add a graceful shutdown signal...
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    if let Err(e) = graceful.await {
+        error!("Server error: {}", e)
+    }
 
     Ok(())
 }
